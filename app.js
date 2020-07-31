86bd6afc6cac0452bf30cdd894e41ececa28d990
ahmedusama25 <ahmed.usama25@hotmail.com>
2020-07-31 17:37:11 +0500

    Added Functionality

--- app.js
+++ app.js
@@ -10,9 +10,6 @@ function submit(){
 
 
 
-
-
-
 //    var A1 = document.getElementsByName("A1").length
 //    for( var i =0;i<<A1.length;i++){
 //        if(A1[i].checked) 
@@ -22,9 +19,50 @@ function submit(){
 
 
 
-
-
-
-
     //console.log(quiz[0].getAttribute("class"))
+}
+
+
+
+function Answers(){
+
+        var Q1= document.getElementById("Q1");
+        var Q2 = document.getElementById("Q2");
+        var Q3 = document.getElementById("Q3");
+        var Q4 = document.getElementById("Q4");
+        var count =0;
+        
+        
+        for(var i=0;i<Q1.childNodes.length;i++){
+            if( Q1.childNodes[i].nodeType == 1){
+                //Question 1 answer
+                if (Q1.childNodes[i].checked == true){
+                    if(Q1.childNodes[i].value == "Quadi-e-Azam"){
+                        count++
+                    }
+                }
+
+                //Question 2 answer
+                if (Q2.childNodes[i].checked == true){
+                    if(Q2.childNodes[i].value == "Charles Babage"){
+                        count++
+                    }
+                }
+
+                //Question 3 answer
+                if (Q3.childNodes[i].checked == true){
+                    if(Q3.childNodes[i].value == "63"){
+                        count++
+                    }
+                }
+                
+                //Question 4 answer
+                if (Q4.childNodes[i].checked == true){
+                    if(Q4.childNodes[i].value == "Arouse Great curiosity in someone"){
+                        count++
+                    }
+                }
+                    console.log(count)       
+            }
+        }
 }
