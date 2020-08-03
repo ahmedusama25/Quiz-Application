7082a1828dd4fe634cd31fed955a6fd10682eeb2
ahmedusama25 <57043086+ahmedusama25@users.noreply.github.com>
2020-08-03 11:56:32 +0500

    Added Functionality
    Added final Score Page.

--- app.js
+++ app.js
@@ -3,66 +3,77 @@ function submit(){
     var email = document.getElementById("email").value;
     var CNIC  = document.getElementById("CNIC").value;
     var quiz = document.getElementsByTagName("div");
-    quiz[0].style.visibility = "hidden";
+    quiz[0].style.display = "none";
     var Questions = document.getElementById("Questions")
     Questions.className += " aa"
     
 
 
-
-//    var A1 = document.getElementsByName("A1").length
-//    for( var i =0;i<<A1.length;i++){
-//        if(A1[i].checked) 
-//                document.getElementById("result").innerHTML
-//                        = "Gender: "+A[i].value;
-//    }
-
-
-
-    //console.log(quiz[0].getAttribute("class"))
 }
 
-
+var count = 0;
 
 function Answers(){
 
-        var Q1= document.getElementById("Q1");
-        var Q2 = document.getElementById("Q2");
-        var Q3 = document.getElementById("Q3");
-        var Q4 = document.getElementById("Q4");
-        var count =0;
-        
-        
-        for(var i=0;i<Q1.childNodes.length;i++){
-            if( Q1.childNodes[i].nodeType == 1){
-                //Question 1 answer
-                if (Q1.childNodes[i].checked == true){
-                    if(Q1.childNodes[i].value == "Quadi-e-Azam"){
-                        count++
-                    }
-                }
+    var Q1= document.getElementById("Q1");
+    var Q2 = document.getElementById("Q2");
+    var Q3 = document.getElementById("Q3");
+    var Q4 = document.getElementById("Q4");
 
-                //Question 2 answer
-                if (Q2.childNodes[i].checked == true){
-                    if(Q2.childNodes[i].value == "Charles Babage"){
-                        count++
-                    }
+    
+    //Answers Checking        
+    for(var i=0;i<Q1.childNodes.length;i++){
+        if( Q1.childNodes[i].nodeType == 1){
+            //Question 1 answer
+            if (Q1.childNodes[i].checked == true){
+                if(Q1.childNodes[i].value == "Quadi-e-Azam"){
+                    count++
                 }
-
-                //Question 3 answer
-                if (Q3.childNodes[i].checked == true){
-                    if(Q3.childNodes[i].value == "63"){
-                        count++
-                    }
-                }
-                
-                //Question 4 answer
-                if (Q4.childNodes[i].checked == true){
-                    if(Q4.childNodes[i].value == "Arouse Great curiosity in someone"){
-                        count++
-                    }
-                }
-                    console.log(count)       
             }
+            //Question 2 answer
+            if (Q2.childNodes[i].checked == true){
+                if(Q2.childNodes[i].value == "Charles Babage"){
+                    count++
+                }
+            }
+            //Question 3 answer
+            if (Q3.childNodes[i].checked == true){
+                if(Q3.childNodes[i].value == "63"){
+                    count++
+                }
+            }
+            
+            //Question 4 answer
+            if (Q4.childNodes[i].checked == true){
+                if(Q4.childNodes[i].value == "Arouse Great curiosity in someone"){
+                    count++
+                }
+            }
+                      
         }
+    }
+    console.log(count) 
+    var Result = document.getElementById("Result")
+    Result.className = "Result"
+    
+    var ResPara = document.getElementById("ResPara")
+    if (count === 4){
+        ResPara.innerHTML = "congrates!"+ name + "You have scored 40 Marks"
+    }    
+    if(count===3){
+        ResPara.innerHTML=name + ", \n You have scored 30 marks "
+    }
+    if(count===2){
+        ResPara.innerHTML=name + ", \n You have scored 20 marks"
+    }
+    if (count==1){
+        ResPara.innerHTML=  + ", \n You have anly been able to score 10 marks"
+    }
+
+    Questions.className="hidden"
+
+    console.log(name)
+
+
+
 }
